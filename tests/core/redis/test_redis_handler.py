65d5b3a16a7db49e924a0e31431932d0c37bac47
John Sigvald Skauge <thejohnskauge@gmail.com>
2018-10-04 21:02:52 +0200

    Removed redis handler test

--- tests/core/redis/test_redis_handler.py
+++ tests/core/redis/test_redis_handler.py
@@ -1,20 +1,20 @@
 from core.redis.redis_handler import RedisHandler
 
-def test_reader():
-    urls = [
-        "www.vg.no",
-        "https://nettavisen.no"
-        "tek.no",
-        "cnn.com",
-        "http://itavisen.no",
-        "http://engadget.com",
-        "http://boingboing.com",
-        "http://ign.com",
-        "https://arstechnica.com/",
-    ]
+# def test_reader():
+#     urls = [
+#         "www.vg.no",
+#         "https://nettavisen.no"
+#         "tek.no",
+#         "cnn.com",
+#         "http://itavisen.no",
+#         "http://engadget.com",
+#         "http://boingboing.com",
+#         "http://ign.com",
+#         "https://arstechnica.com/",
+#     ]
 
-    handler = RedisHandler(host="localhost")
-    handler.write(urls=urls)
+#     handler = RedisHandler(host="localhost")
+#     handler.write(urls=urls)
 
 
 #    saved = handler.read(len(urls))
